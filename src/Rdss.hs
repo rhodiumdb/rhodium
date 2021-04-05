9b278720967032338864946373a5325fe954aedd
chessai <chessai1996@gmail.com>
2021-04-05 13:32:19 -0700

    Creō ā nihilō

---
+++ src/Rdss.hs
@@ -0,0 +1,51 @@
+{-# LANGUAGE DeriveDataTypeable  #-}
+{-# LANGUAGE DeriveFoldable      #-}
+{-# LANGUAGE DeriveFunctor       #-}
+{-# LANGUAGE DeriveGeneric       #-}
+{-# LANGUAGE DeriveTraversable   #-}
+{-# LANGUAGE DerivingStrategies  #-}
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE LambdaCase          #-}
+{-# LANGUAGE PackageImports      #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Rdss where
+
+import "base" Control.Monad
+import "base" Data.Data (Data)
+import "base" Data.List qualified as List
+import "base" GHC.Generics (Generic)
+import "base" Numeric.Natural
+import "containers" Data.Map.Strict (Map)
+import "containers" Data.Map.Strict qualified as Map
+import "containers" Data.Set (Set)
+import "mtl" Control.Monad.State.Class (get, modify, put)
+import "mtl" Control.Monad.State.Strict (State)
+
+--------------------------------------------------------------------------------
+
+type Attr = Int
+
+type AttrPermutation = [Attr]
+
+data Constant
+  = ConstantInt Int
+  | ConstantBool Bool
+  | ConstantBitString [Bool]
+  deriving stock (Eq, Ord, Show, Generic, Data)
+
+--------------------------------------------------------------------------------
+
+data RelAlgebra rel
+  = Not rel
+  | Const [Constant]
+  | Join Natural rel rel
+  | Union rel rel
+  | Project [Attr] rel -- TODO: should be (Set Attr)
+  | Rename AttrPermutation rel
+  | Difference rel rel
+  | Select Attr Constant rel -- Int constants are all we have right now
+  deriving stock (Eq, Ord, Show, Generic)
+  deriving stock (Functor, Foldable, Traversable)
+
+--------------------------------------------------------------------------------
