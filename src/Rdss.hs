51a838d13d33a029009b335877cf615f2b55cbd2
Remy Goldschmidt <taktoa@gmail.com>
2021-04-09 17:00:35 -0700

    Add DataStructure and some comments

--- src/Rdss.hs
+++ src/Rdss.hs
@@ -38,6 +38,11 @@ data Function rel
   = Function -- invariant: must have number of arguments equal to number of columns in the table
       String -- ^ name of function in scope
 
+data Viewed rel
+  = Viewed ![Maybe Attr] !rel
+  deriving stock (Eq, Ord, Show, Generic)
+  deriving stock (Functor, Foldable, Traversable)
+
 data Predicate rel
   = PredicateAnd (Predicate rel) (Predicate rel)
   | PredicateOr (Predicate rel) (Predicate rel)
@@ -48,20 +53,175 @@ data Predicate rel
 
 --------------------------------------------------------------------------------
 
-data Viewed rel
-  = Viewed ![Maybe Attr] !rel
-  deriving stock (Eq, Ord, Show, Generic)
-  deriving stock (Functor, Foldable, Traversable)
-
 data RelAlgebra rel
   = Not                        (Viewed rel)
-  | Join       Natural         (Viewed rel) (Viewed rel)
+  | Join       (Set Attr)      (Viewed rel) (Viewed rel)
   | Union                      (Viewed rel) (Viewed rel)
   | Difference                 (Viewed rel) (Viewed rel)
   | Select     (Predicate rel) (Viewed rel)
   | Map        (Function rel)  (Viewed rel)
   | View                       (Viewed rel)
+  -- TODO: aggregation?
   deriving stock (Eq, Ord, Show, Generic)
   deriving stock (Functor, Foldable, Traversable)
+
+--------------------------------------------------------------------------------
+
+data DataStructure
+  = DataStructure
+    ![TyParam]
+    ![Member]
+    ![Method]
+
+newtype TyParam = TyParam Text
+
+data Member
+  = Member
+    { _memberName :: !Text
+    , _memberType :: !Type
+    }
+
+data Type
+  = TyBasic   !TypeName
+  | TyRow     ![Type]
+  | TyHashSet !Type
+  | TyHashMap !Type !Type
+  | TyTrie    !Type !Type
+  | TyVector  !Type
+
+data Method
+  = Method
+    { _methodName :: !VarName
+    , _methodArgs :: ![(VarName, Type)]
+    , _methodBody :: ![Action]
+    }
+
+data Action
+  = Temp
+  | AssignConstant
+    !VarName  -- ^ variable to assign to
+    !Constant -- ^ constant to assign
+  | IndexRow
+    !VarName -- ^ variable to assign to
+    !VarName -- ^ row to index into
+    !Natural -- ^ index to use
+
+  | CreateHashSet
+    !VarName -- ^ hashset to create
+  | InsertHashSet
+    !VarName -- ^ hashset to modify
+    !VarName -- ^ value to insert
+  | DeleteHashSet
+    !VarName -- ^ hashset to modify
+    !VarName -- ^ value to delete
+  | IterateOverHashSet
+    !VarName             -- ^ hashset to loop over
+    !(VarName -> Action) -- ^ body of loop
+
+  | CreateHashMap
+    !VarName -- ^ hashmap to create
+  | InsertHashMap
+    !VarName -- ^ hashmap to modify
+    !VarName -- ^ key
+    !VarName -- ^ value
+  | DeleteHashMap
+    !VarName -- ^ hashmap to modify
+    !VarName -- ^ key to delete
+  | IterateOverHashMap
+    !VarName                        -- ^ hashmap to loop over
+    !((VarName, VarName) -> Action) -- ^ body of loop
+
+  | CreateTrie
+    !VarName -- ^ trie to create
+  | InsertTrie
+    !VarName -- ^ trie to modify
+    !VarName -- ^ key (string)
+    !VarName -- ^ value
+  | DeleteTrie
+    !VarName -- ^ trie to modify
+    !VarName -- ^ key (string)
+
+newtype TypeName = TypeName Text
+newtype VarName = VarName Text
+
+--------------------------------------------------------------------------------
+
+-- Suppose we have
+--   A = Join [] B C
+-- and we insert a row R into B
+-- then we want to find any row S in C
+-- and insert S ⋈ R into A.
+
+-- Suppose we have
+--   A = Join [i] B C
+-- and we insert a row R into B
+-- then we want to find any row S in C such that S[i] = R[i]
+-- and insert S ⋈ᵢ R into A.
+
+-- Suppose we have
+--   A = Join [i, j] B C
+-- and we insert a row R into B
+-- then we want to find any row S in C such that S[i] = R[i] and S[j] = R[j]
+-- and insert S ⋈ᵢⱼ R into A.
+
+-- Suppose we have
+--   A = Union B C
+-- and we want to insert a row R into B
+-- then we want to insert R into A
+
+-- Suppose we have
+--   A = Difference B C
+-- and we want to insert a row R into B
+-- then we want to insert R into A
+
+-- Suppose we have
+--   A = Difference B C
+-- and we want to insert a row R into C
+-- then we want to delete R from A
+
+-- Suppose we have
+--   A = Select P B
+-- and we want to insert a row R into B
+-- then we check if R satisfies P, and if so, insert it into A
+
+-- Suppose we have
+--   A = Map F B
+-- and we want to insert a row R into B
+-- then we apply F to R and insert the result into A
+
+--------------------------------------------------------------------------------
+
+-- Suppose we have
+--   A = Join attrs B C
+-- and we delete a row R from B
+-- then we want to find any row S in A
+-- where supposing that the attributes of R are x₁, ..., xₙ
+-- and S.x₁ = R.x₁, ..., S.xₙ = R.xₙ
+-- then we should should delete S from A
+
+-- Suppose we have
+--   A = Union B C
+-- and we want to delete a row R from B
+-- then we check if R is in C, and if not, delete R from A
+
+-- Suppose we have
+--   A = Difference B C
+-- and we want to delete a row R from B
+-- then we delete R from A
+
+-- Suppose we have
+--   A = Difference B C
+-- and we want to delete a row R from C
+-- then if B contains R, we add R to A
+
+-- Suppose we have
+--   A = Select P B
+-- and we want to delete a row R from B
+-- then we check if R satisfies P, and if so, delete it from A
+
+-- Suppose we have
+--   A = Map F B
+-- and we want to delete a row R from B
+-- then we delete F(R) from A
 
 --------------------------------------------------------------------------------
