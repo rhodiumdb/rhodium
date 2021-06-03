b72392f80a380063644892b845bff6b0e2b298d0
chessai <chessai1996@gmail.com>
2021-06-03 16:46:24 -0700

    tests: read from filesystem instead of inline strings

---
+++ cpp/src/filesystem.hpp
@@ -0,0 +1,57 @@
+#ifndef RDSS_FILESYSTEM_H_
+#define RDSS_FILESYSTEM_H_
+
+#include <errno.h>
+#include <fcntl.h>
+#include <string>
+#include <filesystem>
+
+#include <absl/status/status.h>
+#include <absl/status/statusor.h>
+
+namespace rdss {
+
+absl::Status ErrNoToStatusWithFilename(
+    int errno_value,
+    const std::filesystem::path& file_name
+) {
+    std::stringstream ss;
+
+    ss << "Error Code: " << errno_value;
+    ss << "\nPath was: " << file_name << "\n";
+    // TODO return an actual error
+    return absl::UnknownError(ss.str());
+}
+
+absl::StatusOr<std::string> GetFileContents(
+    const std::filesystem::path& file_name
+) {
+    // Use POSIX C APIs instead of C++ iostreams to avoid exceptions.
+    std::string result;
+
+    int fd = open(file_name.c_str(), O_RDONLY | O_CLOEXEC);
+    if (fd == -1) {
+        return ErrNoToStatusWithFilename(errno, file_name);
+    }
+
+    char buf[4096];
+    while (ssize_t n = read(fd, buf, sizeof(buf))) {
+        if (n < 0) {
+            if (errno == EAGAIN) {
+                continue;
+            }
+            close(fd);
+            return ErrNoToStatusWithFilename(errno, file_name);
+        }
+        result.append(buf, n);
+    }
+
+    if (close(fd) != 0) {
+        return ErrNoToStatusWithFilename(errno, file_name);
+    }
+    return std::move(result);
+}
+
+} // namespace rdss
+
+#endif // RDS_FILESYSTEM_H_
